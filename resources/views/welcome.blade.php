1e3853f874950d94549774d38df1882392696a2a
Paolo96700 <paolo.falcoapp@gmail.com>
2023-07-04 12:46:18 +0200

    five commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         @vite('resources/js/app.js')
     </head>
     <body>
-        <h1 class="text-center">Ciao da vite a tutti</h1>
+        <h1 class="text-center">Ciao da vite a tutti ciao</h1>
         {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
     </body>
 </html>
