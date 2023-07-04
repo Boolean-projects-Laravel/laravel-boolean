4a52a96dc8a3b36d58b1de87651d5c484797ee64
antonionicolaci <antonionicolaci48@gmail.com>
2023-07-04 13:37:33 +0200

    add basic tag / not finished

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,25 @@
     </head>
     <body>
         <header>
-            <h1>ToDO</h1>
+            <h1 class="align-center">ToDO</h1>
         </header>
-        <main>
-            
+        <main class="offset-2 container-sm">
+            <div class="card container-fluid ">
+                <h2>prova</h2>
+                <span>data fine</span>
+                <div>bottoni</div>
+            </div>
+            <div> {{-- include --}}
+                <h2></h2>
+                <div>
+                    data 
+                    bottoni
+                </div>
+                <div>
+                    descrizxion
+                </div>
+                <img src="" alt="">
+            </div>
         </main>
     </body>
 </html>
