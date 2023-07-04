772713a9624e0127bffcc17d28535752a5489f97
Paolo96700 <paolo.falcoapp@gmail.com>
2023-07-04 13:28:16 +0200

    six commit - populate database

--- database/seeders/TodolistsTableSeeder.php
+++ database/seeders/TodolistsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todolist;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,29 @@ class TodolistsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i <= 20; $i++) {
+            Todolist::create([
+
+                'done' => $faker->boolean(),
+                'expire_date' => $faker->date(),
+                'title' => $faker->sentence('2'),
+
+
+                // $var = $faker->numberBetween(0, 1);
+                // if($var==0) { 
+                //     "image"->"https://picsum.photos/200/300";
+                // };
+
+                // if($var==0) { 
+                //     // "image"->"https://picsum.photos/200/300"
+                // };
+            ]);
+
+
+        }
+        ;
+
     }
 }
